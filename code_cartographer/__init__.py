1b073a97cc8cba659e9e039d3d90aad096205f45
Stochastic Sisyphus <stochastic.sisyphus@example.com>
2025-04-24 17:20:57 -0500

    Expose core functionality in package __init__.py

--- code_cartographer/__init__.py
+++ code_cartographer/__init__.py
@@ -1,3 +1,36 @@
 """Code Cartographer - A tool for visualizing and analyzing code repositories."""
 
 __version__ = "0.1.1"
+
+from code_cartographer.core.analyzer import (
+    ProjectAnalyzer,
+    CodeInspector,
+    ComplexityMetrics,
+    DefinitionMetadata,
+    FileMetadata,
+    generate_markdown,
+    generate_dependency_graph
+)
+
+from code_cartographer.core.variant_analyzer import (
+    VariantAnalyzer,
+    CodeNormalizer,
+    SemanticAnalyzer,
+    VariantGroup,
+    VariantMatch
+)
+
+__all__ = [
+    'ProjectAnalyzer',
+    'CodeInspector',
+    'ComplexityMetrics',
+    'DefinitionMetadata',
+    'FileMetadata',
+    'generate_markdown',
+    'generate_dependency_graph',
+    'VariantAnalyzer',
+    'CodeNormalizer',
+    'SemanticAnalyzer',
+    'VariantGroup',
+    'VariantMatch'
+]
